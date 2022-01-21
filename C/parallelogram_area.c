31ea28c62ccdf28f88c271f02b3759872489a05e
AkshitG <90208584+akshit-g@users.noreply.github.com>
2022-01-21 23:16:28 +0530

    new

--- C/parallelogram_area.c
+++ C/parallelogram_area.c
@@ -1,16 +1,18 @@
-/**************************************
+/*
 Program to Find Area of Parallelogram
-***************************************/
+*/
+
 #include <stdio.h>
- 
+
 int main()
 {
     float base, altitude;
     float area;
  
-    printf("Enter base and altitude of the given Parallelogram: \n ");
-    scanf("%f%f", &base, &altitude);
-    area = base * altitude;
-    printf("Area of Parallelogram is: %.3f\n", area);
+    printf("Enter base and altitude of the given Parallelogram: \n "); // user input
+    scanf("%f %f", &base, &altitude);
+    area = base * altitude; // area calculation
+    printf("Area of Parallelogram is: %.3f\n", area); // string formatting using %f
+    
     return 0;
 }
