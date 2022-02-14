afb9000b93790df372f22ea4da27fb7f95a0861f
AkshitG <90208584+akshit-g@users.noreply.github.com>
2022-02-14 11:10:37 +0530

    lab

--- C/Data Structures Lab/1-2P.c
+++ C/Data Structures Lab/1-2P.c
@@ -5,6 +5,20 @@ Find the matrix of given order (m *  nj;n) is a Spare Matrix or not. Assume that
 the elements of the Matrix are 0.
 */
 #include <stdio.h>
+
+int main()
+{
+    int m, n;
+    printf("Enter the Number of rows and columns in a Matrix \n");
+    scanf("%d %d", &m, &n);
+    int Arr[m][n];  // create matrix
+    input((int *)Arr, m, n);  // function call to input the elements of the Matrix
+    printf("The Matrix is :\n");
+    display((int *)Arr, m, n);  // function call to display the Matrix
+    calculate(m, n, Arr);  // function call to check whether the matrix is a Spare Matrix
+}
+
+
 void input(int *A, int R, int C)  // function to input the elements of the Matrix
 {
     for(int i = 0; i < R; i++)
@@ -42,16 +56,4 @@ void calculate (int R, int C, int A[R][C])  // function to check whether the mat
     printf("The given Matrix is a Sparse Matrix.\n");
     else
     printf("The given Matrix is NOT a Sparse Matrix.\n");
-}
-
-int main()
-{
-    int m, n;
-    printf("Enter the Number of rows and columns in a Matrix \n");
-    scanf("%d %d", &m, &n);
-    int Arr[m][n];  // create matrix
-    input((int *)Arr, m, n);  // function call to input the elements of the Matrix
-    printf("The Matrix is :\n");
-    display((int *)Arr, m, n);  // function call to display the Matrix
-    calculate(m, n, Arr);  // function call to check whether the matrix is a Spare Matrix
 }
