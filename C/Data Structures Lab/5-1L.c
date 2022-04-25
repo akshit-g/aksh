ebccde5edc7a0e22b7447e2bdb1672264cf2a35e
AkshitG <90208584+akshit-g@users.noreply.github.com>
2022-04-25 13:36:46 +0530

    update

---
+++ C/Data Structures Lab/5-1L.c
@@ -0,0 +1,105 @@
+/*
+Akshit Gandotra
+Question 1 (Lab)
+Using Circular Array and functions implement Queue Data structure
+*/
+
+#include <stdio.h>
+
+# define MAX 100 
+int queue[MAX];  // array declaration
+int front = -1;
+int rear = -1;
+
+// function to insert an element in a circular queue
+void enqueue(int element)
+{
+	if(front == -1 && rear == -1)  // condition to check queue is empty
+	{
+		front = 0;
+		rear = 0;
+		queue[rear] = element;
+	}
+	else if((rear + 1) % MAX == front)  // condition to check queue is full
+	{
+		printf("Queue is overflow");
+	}
+	else
+	{
+		rear = (rear + 1) % MAX;  // rear is incremented
+		queue[rear] = element;  // assigning a value to the queue at the rear position.
+	}
+}
+
+// function to delete the element from the queue
+int dequeue()
+{
+	if((front == -1) && (rear == -1))  // condition to check queue is empty
+	{
+		printf("\nQueue is underflow");
+	}
+
+	else if(front == rear)
+	{
+		printf("\nThe dequeued element is %d", queue[front]);
+		front = -1;
+		rear  -1;
+	}
+
+	else
+	{
+		printf("\nThe dequeued element is %d", queue[front]);
+		front = (front + 1) % MAX;
+	}
+}
+
+// function to display the elements of a queue
+void display()
+{
+	int i = front;
+	if(front == -1 && rear == -1)
+	{
+		printf("\n Queue is empty");
+	}
+	else
+	{
+		printf("\nElements in a Queue are :");
+		while(i <= rear)
+		{
+			printf("%d,", queue[i]);
+			i=(i+1)%MAX;
+		}
+	}
+}
+
+int main()
+{
+	int choice = 1, x;
+	
+	while(choice < 4 && choice != 0)  // while loop
+	{
+		printf("\nPress 1: Insert an element");
+		printf("\nPress 2: Delete an element");
+		printf("\nPress 3: Display the element");
+		printf("\nEnter your choice:	");
+		scanf("%d", &choice);
+
+		switch(choice)
+		{
+
+			case 1:
+
+			printf("Enter the element which is to be inserted");
+			scanf("%d", &x);
+			enqueue(x);
+			break;
+			case 2:
+			dequeue();
+			break;
+			case 3:
+			display();
+
+		}
+	}
+	return 0;
+}
