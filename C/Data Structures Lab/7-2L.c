50786f406da8c83be5ba5908a1098075f11ae184
AkshitG <90208584+akshit-g@users.noreply.github.com>
2022-04-27 20:04:30 +0530

    update

--- C/Data Structures Lab/7-2L.c
+++ C/Data Structures Lab/7-2L.c
@@ -6,89 +6,81 @@ using 'Quick Sort' algorithm and write back in another text file.
 */
 
 #include <stdio.h>
-#define Num 100
-static int count = 0;
-
-void ReadFile(int *array)
+int array[50], t=0;
+void read(FILE *ptr)
 {
-	FILE *fp = fopen("d1.txt", "r");
-	
-	while(1)
+    int a = 0;
+    while(!feof(ptr))
     {
-		if(feof(fp))
-        {
-			break;
-		}
-		fscanf(fp, "%d", &array[count]);
-		count++;
-	}
-	
-	fclose(fp);
-	printf("\t**FILE read Successfully**\n");	
-}
-
-void swap(int *array, int i, int j)
-{
-    int temp = array[i];
-    array[i] = array[j];
-    array[j] = temp; 
-}
-int partition (int arr[], int low, int high)
-{
-    int pivot = arr[high];
-    int i = (low - 1);
-    for (int j = low; j <= high - 1; j++)
-    {
-        if (arr[j] < pivot)
-        {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
-
-void QuickSort(int arr[], int low, int high)
-{
-    if (low < high)
-    {
-        int pi = partition(arr, low, high);
-        QuickSort(arr, low, pi - 1);
-        QuickSort(arr, pi + 1, high);
+        fscanf(ptr, "%d", &array[a]);
+        a++;
+        t++;
     }
 }
 
-void DataDisplay(int *array)
+void write()
 {
-	for(int i=0; i<count; i++)
+    FILE *ptr1=fopen("quick.txt", "a");
+    for(int j = 1; j < t; j++)
     {
-		printf("%d ",*(array+i));
-	}
+        fprintf(ptr1, "%d ", array[j]);
+    }
 }
 
-void WriteFile(int *array)
-{
-	FILE *fptr;
-	fptr = fopen("d2.txt","wb");
-	for(int i=0; i<count; i++)
-    {
-		putw(*(array+i), fptr);
-	}
-	fclose(fptr);
+int partition(int start, int end)  
+{  
+    int pivot = array[end];
+    int i = (start - 1);  
+  
+    for (int j = start; j <= end - 1; j++)  
+    {  
+         
+        if (array[j] < pivot)  
+        {  
+            i++;  
+            int t = array[i];  
+            array[i] = array[j];  
+            array[j] = t;  
+        }  
+    }  
+    int t = array[i+1];  
+    array[i+1] = array[end];  
+    array[end] = t;  
+    return (i+1);  
 }
 
-void main()
-{
-	int array[Num];
-	ReadFile(array);
-	
-	printf("Original Data:\n");
-	DataDisplay(array);
-
-    QuickSort(array, count);
-	printf("\n\nSorted Data:\n");
-	DataDisplay(array);
-
-	WriteFile(array);
+void quick(int start, int end)   
+{  
+    if (start < end)  
+    {  
+        int p = partition(start, end); 
+        quick(start, p-1);  
+        quick(p+1, end);  
+    }  
+}  
+void main()  
+{  
+    int n;
+    FILE *ptr = fopen("text1.txt", "r");
+    if(ptr == NULL)
+    {
+        printf("\nNo such file");
+    }
+    else
+    {
+        read(ptr);
+    }
+    printf("\nBeofre Sorting: ");
+    for(int j = 0; j < t-1; j++)
+    {
+        printf("%d ", array[j]);
+    }
+    printf("\n");
+    quick(0, t-1);
+    write();
+    printf("After Sorting: ");
+    for(int j = 1; j < t; j++)
+    {
+        printf("%d ", array[j]);
+    }
 }
