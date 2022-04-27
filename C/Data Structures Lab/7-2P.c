eaeb3ee5d683c1193cc01da373070921682abf45
AkshitG <90208584+akshit-g@users.noreply.github.com>
2022-04-27 20:16:51 +0530

    update

--- C/Data Structures Lab/7-2P.c
+++ C/Data Structures Lab/7-2P.c
@@ -6,100 +6,110 @@ using 'Merge Sort' algorithm and write back in another text file.
 */
 
 #include <stdio.h>
-#define Num 100
-static int count = 0;
-
-void ReadFile(int *array) {
-	FILE *fp = fopen("d1.txt","r");
+int array[50], i = 0, n1, n2;
+int L[25], R[25];
+void read(FILE *ptr)
+{
+	int a = 0;
+	while(!feof(ptr))
+	{
+		fscanf(ptr, "%d", &array[a]);
+		a++;
+		i++;
+	}
+}
+void write()
+{
+	FILE *ptr1 = fopen("merge.txt", "a");
+	for(int j = 1; j < i; j++)
+	{
+		fprintf(ptr1, "%d ", array[j]);
+	}
+}
+void merge(int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
 	
-	while(1) {
-		if(feof(fp)) {
-			break;
+	// create temporary arrays
+	int L[n1], R[n2];
+
+	// copy data to temp arrays L[] and R[]
+	for (i = 0; i < n1; i++)
+		L[i] = array[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = array[m + 1 + j];
+		
+
+	i = 0;
+	j = 0;
+	k = l;
+	while (i < n1 && j < n2) 
+	{
+		if (L[i] <= R[j]) 
+		{
+			array[k] = L[i];
+			i++;
 		}
-		fscanf(fp, "%d", &array[count]);
-		count++;
+		else 
+		{
+			array[k] = R[j];
+			j++;
+		}
+		k++;
 	}
-	
-	fclose(fp);
-	printf("\t**FILE read Successfully**\n");	
-}
-
-void swap(int *array, int i, int j) {
-    int temp = array[i];
-    array[i] = array[j];
-    array[j] = temp; 
-}
-
-void merge(int arr[], int l, int m, int r) {
-    	int i, j, k;
-    	int n1 = m - l + 1;
-    	int n2 = r - m;
-    	int L[n1], R[n2];
-for (i = 0; i < n1; i++)
-       	L[i] = arr[l + i];
-    	for (j = 0; j < n2; j++)
-        	R[j] = arr[m + 1 + j];
-i = 0; 
-    	j = 0; 
-    	k = l; 
-    	while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        }
-        else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-  
-while (i < n1) {
-       	 arr[k] = L[i];
-        	i++;
-        	k++;
-    } while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
-  
-void MergeSort(int arr[], int l, int r) {
-    if (l < r) {  
-int m = l + (r - l) / 2;
-        	MergeSort(arr, l, m);
-        	MergeSort(arr, m + 1, r);
-  
-        merge(arr, l, m, r);
-    }
-}
-
-void DataDisplay(int *array) {
-	for(int i=0; i<count; i++) {
-		printf("%d ",*(array+i));
+	/* Copy the remaining elements of L[], if there
+	are any */
+	while (i < n1)
+	{
+		array[k] = L[i];
+		i++;
+		k++;
+	}
+	/* Copy the remaining elements of R[], if there
+	are any */
+	while (j < n2)
+	{
+		array[k] = R[j];
+		j++;
+		k++;
 	}
 }
-
-void WriteFile(int *array) {
-	FILE *fptr;
-	fptr = fopen("d2.txt","wb");
-	for(int i=0; i<count; i++) {
-		putw(*(array+i), fptr);
+void mergeSort(int l, int r)
+{
+	if (l < r) {
+		// Same as (l+r)/2, but avoids overflow for
+		// large l and h
+		int m = l + (r - l) / 2;
+		// Sort first and second halves
+		mergeSort(l, m);
+		mergeSort(m + 1, r);
+		merge(l, m, r);
 	}
-	fclose(fptr);
 }
-
-void main() {
-	int array[Num];
-	ReadFile(array);
-	
-	printf("Original Data:\n");
-	DataDisplay(array);
-
-     MergeSort(array, count);
-	printf("\n\nSorted Data:\n");
-	DataDisplay(array);
-
-	WriteFile(array);
+void main()
+{
+	FILE *ptr = fopen("text1.txt", "r");
+	if(ptr == NULL)
+	{
+		printf("\nNo such file");
+	}
+	else
+	{
+		read(ptr);
+	}
+	printf("\nBefore Sorting: ");
+	for(int j = 0; j < i-1; j++)
+	{
+		printf("%d ", array[j]);
+	}
+	printf("\n");
+	mergeSort(0, i-1);
+	write();
+	printf("After Sorting: ");
+	for(int j = 1; j < i; j++)
+	{
+		printf("%d ", array[j]);
+	}
 }
